2cbde9f90354fb6059ec617517656da4a1b7d8de
YCXK <695881796@qq.com>
2026-01-17 03:22:23 +0800

    Add files via upload

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("C:\\Users\\ASUS\\Desktop\\docusaurus-v3-new\\unconscious-trampling\\.docusaurus\\docusaurus-plugin-css-cascade-layers\\default\\layers.css"),
+  require("C:\\Users\\ASUS\\Desktop\\docusaurus-v3-new\\unconscious-trampling\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("C:\\Users\\ASUS\\Desktop\\docusaurus-v3-new\\unconscious-trampling\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("C:\\Users\\ASUS\\Desktop\\docusaurus-v3-new\\unconscious-trampling\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("C:\\Users\\ASUS\\Desktop\\docusaurus-v3-new\\unconscious-trampling\\src\\css\\custom.css"),
+];
