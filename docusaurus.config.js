4249bf28e395f3e33efe4838ab50dc46622e42d8
YCXK <695881796@qq.com>
2026-01-17 14:24:53 +0800

    Update tagline in docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: '无意识',
-  tagline: 'Dinosaurs are cool',
+  tagline: '顾名思义',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
